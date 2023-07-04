e7701d63936d27f6c81e338a2c298aea5df27b85
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-07-03 23:04:22 -0500

    view: User screen

--- src/view/screens/User.tsx
+++ src/view/screens/User.tsx
@@ -1,38 +1,22 @@
-import { Icon, useTheme } from "@ui-kitten/components";
-import { useRef, useState } from "react";
-import { Keyboard, View } from "react-native";
+import { Icon, Text, useTheme } from "@ui-kitten/components";
+import { View } from "react-native";
 import ActionButton from "../components/ActionButton";
-import BookInput from "../components/BookInput";
-import DatePicker from "../components/DatePicker";
 import FormInput from "../components/FormInput";
-import InputCarousel from "../components/InputCarousel";
 import RoundButton from "../components/RoundButton";
-import StatusButton from "../components/StatusButton";
-import StatusTouch from "../components/StatusTouch";
 import { globalStyles as styles } from "../styles/styles";
 
 export default function User() {
-    const [isbn, setISBN] = useState('')
-    const [star, setStar] = useState(false)
-    let inputRef = useRef<any>()
-
-    const starHandler = () => {
-        setStar(star => !star)
-        Keyboard.dismiss()
-        inputRef.current!.blur()
-    }
-
     const theme = useTheme()
-    const fontColor = theme['background-alternative-color-4']
-    const AddIcon = () => <Icon name="person-add" fill={fontColor} height="35" width="35" />;
-    const CloseIcon = () => <Icon name="log-out" fill={fontColor} height="35" width="35" />;
-    const SaveIcon = () => <Icon name="save" fill={fontColor} height="35" width="35" />;
-    const StarIcon = <Icon name="star" fill={star ? 'gold' : 'darkgray'} height="35" width="35" />;
-    const EyeIcon = <Icon name="eye" fill={star ? 'turquoise' : 'darkgray'} height="35" width="35" />;
-    const TrashIcon = () => <Icon name="trash-2" fill={fontColor} height="35" width="35" />;
+    const AddIcon = () => <Icon name="person-add" fill="black" height="35" width="35" />;
+    const CloseIcon = () => <Icon name="log-out" fill="black" height="35" width="35" />;
+    const EditIcon = () => <Icon name="edit" fill="white" height="30" width="30" />;
+    const StopIcon = () => <Icon name="slash" fill="white" height="30" width="30" />;
+    const TrashIcon = () => <Icon name="trash-2" fill={"white"} height="35" width="35" />;
+    const SaveIcon = () => <Icon name="save" fill={"white"} height="35" width="35" />;
     return <View style={[styles.common, { flex: 1 }]}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+        <View style={{ flex: 1, width: '80%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
             <RoundButton
+                disabled
                 icon={AddIcon}
                 backgroundColor={theme['color-warning-500']}
             />
@@ -41,50 +25,40 @@ export default function User() {
                 backgroundColor={theme['color-danger-600']}
             />
         </View>
-        <View style={{ width: "50%" }}>
-            <DatePicker />
+        <View>
+            <Icon name="person-outline" fill={theme['background-alternative-color-4']} height="100" width="100" />
+            <Text
+                style={{ fontSize: 30, fontFamily: "serif", fontStyle: "italic", textAlign: "center", textTransform: "uppercase" }}
+            >
+                Admin
+            </Text>
         </View>
-        <View style={{ width: "90%" }}>
-            <BookInput
-                ref={input => { if (inputRef) inputRef.current = input }}
-                title={"ISBN"}
-                disabled={star}
-                onChangeText={(input) => setISBN(input)}
-            />
-            <BookInput
-                textarea
-                ref={input => { if (inputRef) inputRef.current = input }}
-                title={"Descripción"}
-                disabled={star}
-                onChangeText={(input) => setISBN(input)}
-            />
-        </View>
-        <View style={[{ height: 50, marginVertical: 20, backgroundColor: 'black' }]}>
-            <InputCarousel data={"12 Rules For Life An Antidote to Caos, the Beginning and the End."} />
-        </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
-            <ActionButton
-                icon={SaveIcon}
-                backgroundColor={theme['color-success-500']}
-            />
-            <View style={[styles.common, { width: 'auto', marginHorizontal: 5 }]}>
-                <StatusTouch title="Más vendido" icon={StarIcon} height={40} width={40} activeOpacity={1} backgroundColor={'transparent'} onPressIn={starHandler} />
-                <StatusTouch icon={EyeIcon} height={40} width={40} activeOpacity={1} backgroundColor={'transparent'} onPressIn={starHandler} />
-            </View>
-            <ActionButton
-                icon={TrashIcon}
-                backgroundColor={theme['color-danger-500']}
-            />
-        </View>
-        <View style={{ width: '80%' }}>
+        <View style={{ flex: 2, width: '80%' }}>
             <FormInput isTop title="Usuario" placeholder="Usuario" />
             <FormInput title="Nombre" placeholder="Nombre" />
             <FormInput isBottom inputMode="email" title="Email" placeholder="Email" />
         </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
-            <StatusButton textLeft captionText={"% "} captionFontColor={theme['background-alternative-color-1']}>
-                12
-            </StatusButton>
+        <View style={{ flex: 1, width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly' }}>
+            <ActionButton
+                disabled
+                icon={EditIcon}
+                backgroundColor={theme['color-warning-500']}
+            />
+            <ActionButton
+                disabled
+                icon={StopIcon}
+                backgroundColor={theme['color-warning-500']}
+            />
+            <ActionButton
+                disabled
+                icon={TrashIcon}
+                backgroundColor={theme['color-danger-500']}
+            />
+            <ActionButton
+                disabled
+                icon={SaveIcon}
+                backgroundColor={theme['color-success-500']}
+            />
         </View>
     </View >
 }
